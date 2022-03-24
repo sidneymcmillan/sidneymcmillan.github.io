7e785064ec28092971c79e8d4144f6f941bf619c
sidneymcmillan <sidney.mcmillan01@gmail.com>
2022-03-24 13:28:55 +0000

    finished base code

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,9 +16,17 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "sawblade", "x": 400, "y": groundY },
-                { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "sawblade", "x": 400, "y": groundY -50 },
+                { "type": "sawblade", "x": 600, "y": groundY -50 },
+                { "type": "sawblade", "x": 900, "y": groundY -50 },
+
+                { "type": "enemy", "x": 300, "y": groundY -50 },
+                { "type": "enemy", "x": 500, "y": groundY -50 },
+                { "type": "enemy", "x": 700, "y": groundY -50 },
+
+                { "type": "reward", "x": 200, "y": groundY -50 },
+                { "type": "reward", "x": 400, "y": groundY -50 },
+                { "type": "reward", "x": 800, "y": groundY -50 },
             ]
         };
         window.levelData = levelData;
@@ -44,9 +52,74 @@ var level01 = function (window) {
 
         }
 
-        createSawBlade(400, 370);
-        createSawBlade(600, 500);
-        createSawBlade(800, 345);
+    
+
+        function createEnemy (x, y){
+            var enemy = game.createGameItem('enemy',25);
+            var redSquare = draw.rect(50,50,'red');
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare);
+
+            enemy.x = x;
+            enemy.y = y;
+
+            game.addGameItem(enemy);
+
+            enemy.velocityX = -1;
+
+            enemy.onPlayerCollision = function() {
+                console.log('The enemy has hit Halle');
+                game.changeIntegrity(-10);
+            };
+            
+            enemy.onProjectileCollision = function() {
+                console.log('The enemy has hit Halle');
+                game.changeIntegrity(10);
+                game.increaseScore(10);
+                enemy.fadeOut();
+            };
+        }
+        
+        function createReward (x, y){
+            var reward = game.createGameItem('reward',25);
+            var blueSquare = draw.rect(50,50,'blue');
+            blueSquare.x = -25; n
+            blueSquare.y = -25;
+            reward.addChild(blueSquare);
+
+            enemy.x = x;
+            enemy.y = y;
+
+            game.addGameItem(reward);
+
+            reward.velocityX = -1;
+
+            reward.onPlayerCollision = function() {
+                console.log('Hallie has collected a reward');
+                game.changeIntegrity(10);
+                game.increaseScore(100);
+            };
+            
+        }
+        
+
+        for (var i = 0; i < levelData.gameItems.length; i++){
+           var gameItem = levelData.gameItems[i];
+
+            if (gameItem.type === "sawblade"){
+                createSawBlade(gameItem.x, gameItem.y);
+            }
+
+            if (gameItem.type === "enemy"){
+                createEnemy(gameItem.x, gameItem.y);
+            }
+
+            if (gameItem.type === "reward"){
+                createReward(gameItem.x, gameItem.y);
+            }
+        }
+
 
 
         // DO NOT EDIT CODE BELOW HERE
